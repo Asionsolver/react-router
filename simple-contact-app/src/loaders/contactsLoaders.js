3596e7b3723cade72f6249d1580ef43174ae7b6b
asionsolver <asionsolver274@gmail.com>
2024-02-28 08:14:03 +0600

    Finally Display data in this app

--- simple-contact-app/src/loaders/contactsLoaders.js
+++ simple-contact-app/src/loaders/contactsLoaders.js
@@ -1,7 +1,14 @@
-import {  getContacts } from "../contacts";
+// import localforage from "localforage";
+import { getContact, getContacts } from "../contacts";
 
 
 export async function getContactsLoader() {
+  // localforage.clear();
   const contacts = await getContacts();
   return { contacts };
+}
+
+export async function getContactLoader({ params }) {
+  const contact = await getContact(params.contactId);
+  return { contact };
 }
--- simple-contact-app/src/main.jsx
+++ simple-contact-app/src/main.jsx
@@ -5,16 +5,28 @@ import "./index.css";
 import Root from "./Root";
 import ErrorPage from "./Error";
 import Contact from "./Contact";
+import { getContactLoader, getContactsLoader } from "./loaders/contactsLoaders";
+import { createContactAction, editContactAction } from "./action/contactsActions";
+import EditContact from "./EditContact";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    loader: getContactsLoader,
+    action: createContactAction,
     children: [
       {
         path: "/contacts/:contactId",
         element: <Contact />,
+        loader: getContactLoader,
+      },
+      {
+        path: "/contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: getContactLoader,
+        action: editContactAction,
       },
     ],
   },
